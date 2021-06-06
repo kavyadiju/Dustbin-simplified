7e753be61f1c193f3836c86b2b08a341598fe037
kavyadiju <70419997+kavyadiju@users.noreply.github.com>
2021-06-06 19:07:49 +0530

    Update paper.js

--- paper.js
+++ paper.js
@@ -8,7 +8,7 @@ class paper
 			friction:0.5,
 			density:1.2
 			}
-		this.image = loadImage("paper.png");
+		
 
 		this.r=r
 		this.body=Bodies.circle(x,y, this.r/2, options);
@@ -19,7 +19,7 @@ class paper
 	{	
 	    	var paperpos=this.body.position;		
 			imageMode(CENTER);
-			image(this.image, paperpos.x,paperpos.y,this.r, this.r)
+			
 	
 	}
 
